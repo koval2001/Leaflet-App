4c8adc9db613c70c3fac3bbbbdad3e3e8f88f5f8
koval2001 <d.koval@geniusee.com>
2023-02-21 22:57:14 +0200

    added markers & custom marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,31 +1,75 @@
 // @ts-ignore
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
+import L, { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-// @ts-ignore
-import icon from 'leaflet/dist/images/marker-icon.png';
-// @ts-ignore
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import { HotChocolate } from '../../domain/domain';
+import './style.css';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+const list: HotChocolate[] = [
+  {
+    productName: "Varm belgisk sjokolade",
+    englishProductName: "Belgian hot chocolate",
+    vendor: "Steam kaffebar",
+    location: "Jernbanetorget 1, Østbanehallen",
+    lat: 59.91088362120013,
+    lon: 10.752799203777597,
+  },
+  {
+    productName: "Varm sjokolade",
+    englishProductName: "Hot chocolate",
+    vendor: "Kaffebrenneriet",
+    location: "Karl Johans gate 7, Arkaden",
+    lat: 59.91181003626315,
+    lon: 10.747782602301388,
+  },
+  {
+    productName: "Sjokolade på pinne",
+    englishProductName: "Hot chocolate on a stick",
+    vendor: "Espresso House",
+    location: "Jernbanetorget 1, Østbanehallen",
+    lat: 59.91201090441835,
+    lon: 10.751298468298101,
+    description: "Seasonally available",
+  },
+];
+
+const icon: L.DivIcon = L.divIcon({
+  className: "hot-chocolate-icon",
+  iconSize: [30, 30],
+  iconAnchor: [0, 0],
+  popupAnchor: [15, 0],
 });
 
-L.Marker.prototype.options.icon = DefaultIcon;
+const position: LatLngExpression = [59.91174337077401, 10.750425582038146];
+const zoom: number = 15;
 
 const Map = () => (
   <div>
-    <MapContainer style={{ height: '100vh', width: '100wh' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer style={{ height: '100vh', width: '100wh' }} center={position} zoom={zoom} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-            I am a simple map.
-        </Popup>
-      </Marker>
+        {list.map((item, index) => (
+          <Marker
+            icon={icon}
+            key={index}
+            position={[item.lat, item.lon]}
+            title={`${item.englishProductName} at ${item.vendor}`}
+          >
+            <Popup>
+              <strong>
+                {item.englishProductName} at {item.vendor}
+              </strong>
+              <br />
+              <p>
+                Look for <strong>{item.productName}</strong> on the menu.
+              </p>
+              <p>{item.location}</p>
+              {item.description && <em>{item.description}</em>}
+            </Popup>
+          </Marker>
+        ))}
     </MapContainer>
   </div>
 );
